d88cd56ebf61a36dfde03086a1257b230072a440
himanshu010 <himanshuaswal@yahoo.com>
2020-03-10 17:48:56 +0530

    finally working

--- folderstruct.js
+++ folderstruct.js
@@ -1,24 +1,34 @@
-var fs = require('fs'),
- path=require('path');
- var FolderNamesArray=[];
- var FolderNamesArray1;
-//  var spacing="";
- function crawl(spacing,dir){
-    var folderName=path.basename(dir);
-    FolderNamesArray.push(folderName);
-    //  console.log(spacing+folderName+"\n");
-     var files= fs.readdirSync(dir);
-     for(var x in files){
-         //ignored folders
-         if(files[x]!==".git"&&files[x]!=="New folder (2)"){
-         var next = path.join(dir,files[x]);
-         if (fs.lstatSync(next).isDirectory()===true) {
-            crawl(spacing+"    ",next);
-         }
-        }
-     }
- }
- crawl("",__dirname);
- console.log(FolderNamesArray);
-// var dir=path.basename(__dirname);
-// console.log(dir);
+var fs = require('fs')
+var path = require("path");
+var util = require("util");
+
+
+var logFile = fs.createWriteStream("log.md", { flags: "w" });
+var logStdout = process.stdout;
+
+console.log = function() {
+  logFile.write(util.format.apply(null, arguments) + "\n");
+  logStdout.write(util.format.apply(null, arguments) + "\n");
+};
+console.error = console.log;
+
+
+var FolderNamesArray = [];
+var level = 0;
+var spacing = "";
+function crawl(spacing, dir, level) {
+  var folderName = path.basename(dir);
+  FolderNamesArray.push(folderName);
+  console.log(spacing + "├───" + folderName + "\n");
+  var files = fs.readdirSync(dir);
+  for (var x in files) {
+    //ignored folders
+    if (files[x] !== ".git" && files[x] !== "New folder (2)") {
+      var next = path.join(dir, files[x]);
+      if (fs.lstatSync(next).isDirectory() === true) {
+        crawl(spacing + "    ", next, level + 1);
+      }
+    }
+  }
+}
+crawl(spacing, __dirname, level);
---
+++ log.md
@@ -0,0 +1,18 @@
+├───folder-structure
+
+    ├───himanshu
+
+        ├───New folder2-1
+
+        ├───New folder2-2
+
+    ├───New folder
+
+    ├───New folder (3)
+
+        ├───aa
+
+            ├───ss
+
+    ├───New folder (4)
+
